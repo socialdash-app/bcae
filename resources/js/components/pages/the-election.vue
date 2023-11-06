38984f148715596c05409fb2b53a276bc17128a8
Thomas <zawlintun@robust.best>
2023-11-06 12:19:23 +0700

    update

--- resources/js/components/pages/the-election.vue
+++ resources/js/components/pages/the-election.vue
@@ -1,29 +1,46 @@
 <template>
-    <svg id="map" height="560" width="340">
-        <g class="map" fill="#b3b3b3" stroke="black" stroke-width="0.01"></g>
-        <g class="details" fill="orange" stroke="black" stroke-width="0.01"></g>
-    </svg>
+    <div id="container" class="w-full h-full flex p-10 items-start overflow-auto">
+        <div class="w-1/2 self-center h-5/6 absolute flex flex-col items-center">
+            <svg id="map" class="w-full h-full">
+                <g class="map" fill="#b3b3b3" stroke="black" stroke-width="0.01"></g>
+                <g class="details" fill="orange" stroke="black" stroke-width="0.01"></g>
+            </svg>
+            <div class="flex flex-col absolute bottom-0 left-0 gap-y-4">
+                <button class="w-64 h-16 border bg-amber-300"></button>
+                <button class="w-64 h-16 border bg-blue-300"></button>
+            </div>
+        </div>
+        <div class="w-1/2"></div>
+        <div class="w-1/2 gap-y-24 flex flex-col">
+            <div class="trigger w-full border rounded bg-gray-200 h-[40vh]"></div>
+            <div class="trigger w-full border rounded bg-gray-200 h-[40vh]"></div>
+            <div class="trigger w-full border rounded bg-gray-200 h-[40vh]"></div>
+        </div>
+    </div>
+
 </template>
 
 <script setup>
 import {onMounted, reactive} from 'vue';
 import * as d3 from 'd3';
+import anime from "animejs";
 
 const data = reactive({});
-const init = ()=>{
+const init = () => {
     const svg = d3.select('svg#map');
-    const width = +svg.attr('width');
-    const height = +svg.attr('height');
-    const options = {
-        x: -7.92 * width,
-        y: height + 290,
-        scale: 1700,
-    };
+    // const width = +svg.attr('width');
+    // const height = +svg.attr('height');
+    const width = svg.node().getBoundingClientRect().width;
+    const height = svg.node().getBoundingClientRect().height;
+    // const options = {
+    //     x: -7.92 * width,
+    //     y: height + 290,
+    //     scale: 1700,
+    // };
     Promise.all([
-        d3.json( '/assets/states-and-regions.json'),
+        d3.json('/assets/states-and-regions.json'),
     ]).then(([json]) => {
-
-        var projection = d3.geoEquirectangular().scale(options.scale).translate([options.x,options.y]);
+        var projection = d3.geoEquirectangular().fitSize([width, height], json)
         var path = d3.geoPath().projection(projection);
 
         const g = d3.select('#map g.map');
@@ -31,16 +48,114 @@ const init = ()=>{
             .data(json.features)
             .enter()
             .append('path')
-            .attr('d',(d)=>{
-                return path(d)})
-            .style("fill", "gray")
+            .attr('d', (d) => {
+                return path(d)
+            })
+            .style("fill", "#e3e3e3")
             .style("stroke-width", "1")
-            .style("stroke", "black");
+            .style("stroke", "#313131");
     });
+
+
 }
-onMounted(()=>{
-  init()
+onMounted(() => {
+    init()
+
+    let prevRatio = 0.0;
+
+    const intersectionCallback = (entries) => {
+        entries.forEach((entry) => {
+            prevRatio = entry.intersectionRatio;
+            console.log(entry, entry.intersectionRatio)
+        });
+    }
+    let container = document.getElementById('container')
+    let divs = container.querySelectorAll('.trigger');
+    let triggers = [{
+        targets: divs[0],
+        backgroundColor: '#000',
+        scrollTrigger: {
+            trigger: divs[0],
+        }
+    },
+        {
+            targets: divs[1],
+            backgroundColor: '#f10202',
+            scrollTrigger: {
+                trigger: divs[1],
+            }
+        }, {
+            targets: divs[2],
+            backgroundColor: '#02cdf1',
+            scrollTrigger: {
+                trigger: divs[2],
+                start: 'top bottom'
+            }
+        }]
+
+    function getScrollOffset(position) {
+        if (position.includes('%')) return parseInt(position.substring(0, position.length - 1)) / 100;
+        if (parseInt(position)) return parseInt(position);
+        switch (position) {
+            case 'top' :
+                return 0;
+            case 'bottom':
+                return 1;
+            case 'center':
+                return 0.5;
+            default:
+                return null;
+        }
+    }
+
+    let scrollTrigger = (element, triggers) => {
+        triggers.forEach((trigger) => {
+            trigger.animations = {};
+            trigger.hasTriggered = false;
+            Object.keys(trigger).forEach((key) => {
+                if (!['targets', 'scrollTrigger', 'animations'].includes(key)) {
+                    trigger.animations[key] = trigger[key]
+                }
+            })
+            let triggerRect = trigger.scrollTrigger.trigger.getBoundingClientRect();
+            // start trigger offset and scroll offset
+            let start = trigger.scrollTrigger.start ?? 'top center';
+            start = start.split(' ');
+            if (start.length < 2) {
+                throw new Error('Start must be in the format of "triggerOffset scrollOffset"')
+            }
+            trigger.startTriggerOffset = triggerRect.top + triggerRect.height * getScrollOffset(start[0]);
+            trigger.startScrollPosition = start[1];
+            let end = trigger.scrollTrigger.end ?? 'bottom center';
+            end = end.split(' ');
+            if (end.length < 2) {
+                throw new Error('end must be in the format of "triggerOffset scrollOffset"')
+            }
+            trigger.endScrollPosition = end[1];
+            trigger.endTriggerOffset = triggerRect.top + triggerRect.height * getScrollOffset(end[0]);
+            // trigger.scrollerOffset =
+        })
+        let scrollerHeight = element.getBoundingClientRect().height;
+        element.addEventListener('scroll', (e) => {
+            triggers.filter((trigger) => {
+                let startScrollerOffset = element.scrollTop + scrollerHeight * getScrollOffset(trigger.startScrollPosition);
+                let endScrollerOffset = element.scrollTop + scrollerHeight * getScrollOffset(trigger.endScrollPosition);
+                return trigger.startTriggerOffset <= startScrollerOffset && endScrollerOffset >= trigger.endTriggerOffset
+            }).forEach((trigger) => {
+                let a = anime({
+                    targets: trigger.targets,
+                    ...trigger.animations,
+                    autoplay: false,
+                })
+                trigger.hasTriggered ? a.reverse() : a.play();
+                trigger.hasTriggered = !trigger.hasTriggered;
+            });
+        })
+    }
+
+    scrollTrigger(container, triggers);
 })
+
 </script>
 
 <style scoped>
